eabad7c888873ad2f8b0b17930afcb77e636ad81
Henri Qiu <qiu.henri@gmail.com>
2019-02-08 03:51:06 +0100

    Updates

---
+++ precache-manifest.7d5c44d48f4d413e8e341eb15f9979db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9668a5bfa44f0fce6b31",
+    "url": "/ca_react_dapp/static/css/main.b5b7df8e.chunk.css"
+  },
+  {
+    "revision": "9668a5bfa44f0fce6b31",
+    "url": "/ca_react_dapp/static/js/main.9668a5bf.chunk.js"
+  },
+  {
+    "revision": "c8449cdcee9d6922bf6b",
+    "url": "/ca_react_dapp/static/js/1.c8449cdc.chunk.js"
+  },
+  {
+    "revision": "46c2ae7ac89219604609",
+    "url": "/ca_react_dapp/static/js/runtime~main.46c2ae7a.js"
+  },
+  {
+    "revision": "71f2595b1f21c81731e448e1e0858b8a",
+    "url": "/ca_react_dapp/index.html"
+  }
+];
